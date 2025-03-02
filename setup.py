5fea91286007e625ace5358786001f39e55ed556
Marcos Lino <marcos.lino@ssense.com>
2025-03-02 10:58:27 -0500

    new package

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    package_dir={"polars_jdbc_tools": "src/polars_jdbc_tools"},
-    packages=["polars_jdbc_tools"],
+    name="polars_jdbc_tools",
+    version="0.1.0",
+    description="A comprehensive toolkit for working with JDBC connections in AWS environments using Polars",
+    packages=find_packages(),
+    python_requires=">=3.8",
+    install_requires=[
+        "boto3>=1.28.0",
+        "polars>=0.18.0",
+        "sqlalchemy>=2.0.0",
+    ],
 )
